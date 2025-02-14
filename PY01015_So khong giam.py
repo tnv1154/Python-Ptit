b669893f3698ce52f4bc00e5bcd3fad9ac6085ee
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-02-14 16:32:26 +0700

    14/2/2025

---
+++ PY01015_So khong giam.py
@@ -0,0 +1,13 @@
+
+def check(s):
+    for i in range(len(s) - 1):
+        if s[i] > s[i+1]:
+            return False
+    return True
+
+for i in range(int(input())):
+    s = input()
+    if check(s):
+        print("YES")
+    else :
+        print("NO")
