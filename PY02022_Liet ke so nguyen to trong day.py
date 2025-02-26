b43ac037db8bfedc73f351278ddebd5a28cd9e58
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-02-26 10:16:25 +0700

    26/2/2025

--- PY02022_Liet ke so nguyen to trong day.py
+++ PY02022_Liet ke so nguyen to trong day.py
@@ -1,4 +1,6 @@
+import math
 
+#Sang so nguyen to
 prime = [1] * 1000001
 def sieve():
     prime[0] = prime[1] = 0
@@ -6,7 +8,20 @@ def sieve():
         if prime[i]:
             for j in range(i * i, 1000001, i):
                 prime[j] = 0
-
+#kiem tra so nguyen to toi uu
+"""
+def nto(n):
+    if n < 2:
+        return False
+    if n < 4 :
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, int(math.sqrt(n)) + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+    return True
+"""
 sieve()
 n = int(input())
 a = list(map(int, input().split()))
