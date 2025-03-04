1c7de06a0cc3d94e2570a2bc833f521dccbb021f
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-04 17:25:59 +0700

    04/03/2025

---
+++ PY02040_Ma tran 2.py
@@ -0,0 +1,23 @@
+
+n = int(input())
+a = []
+for i in range(n):
+    b = list(map(int, input().split()))
+    a.append(b)
+k = int(input())
+sumTren = 0
+sumDuoi = 0
+for i in range(n):
+    for j in range(n):
+        if j < n - i - 1:
+           sumTren += a[i][j]
+        elif j > n - i -1:
+            sumDuoi += a[i][j]
+sum = abs(sumTren - sumDuoi)
+if sum <= k:
+    print("YES")
+else :
+    print("NO")
+print(sum)
+
+
