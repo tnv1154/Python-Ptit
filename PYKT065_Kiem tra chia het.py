922f11054d0cf32e44585f949b71f9a771d76e53
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-08 22:34:08 +0700

    08/03/2025

--- PYKT065_Kiem tra chia het.py
+++ PYKT065_Kiem tra chia het.py
@@ -1,1 +1,19 @@
 
+
+def check(i, n):
+    for j in range(2, n):
+        if i % j == 0:
+            return False
+    return True
+
+while True:
+    s = input()
+    if s == "-1":
+        break
+    l, r = map(int, s.split())
+    n = int(input())
+    cnt = 0
+    for i in range(l, r + 1):
+        if check(i, n):
+            cnt += 1
+    print(cnt)
