8f40b0b3181e98d816e8b697aba263cc085ef904
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-09 23:45:50 +0700

    09/03/2025

--- PY01021_Tinh tong cac chu so.py
+++ PY01021_Tinh tong cac chu so.py
@@ -1,2 +1,14 @@
 
-
+for i in range(int(input())):
+    s = input()
+    sum = 0
+    al = ""
+    for j in s:
+        if j.isdigit():
+            sum += int(j)
+        else:
+            al += j
+    al = sorted(al)
+    for j in al:
+        print(j, end="")
+    print(sum)
