9d3464fb70ae248836f0403a9c0f73e335cb3d97
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-10 16:36:59 +0700

    10/03/2025

--- PY01034_Doi cho cac chu so.py
+++ PY01034_Doi cho cac chu so.py
@@ -1,13 +1,27 @@
 
+def mind(s, i):
+    tmp = i
+    for j in range(i+1, len(s)):
+        if int(s[j]) < int(s[i]):
+            if tmp == i:
+                tmp = j
+            elif s[tmp] < s[j]:
+                tmp = j
+    if s[tmp] < s[i]:
+        return tmp
+    return -1
+
 for x in range(int(input())):
     s = input()
+    n = len(s)
     ans = ""
-    for i in range(1, len(s), -1):
-        if s[i] < s[i-1]:
-           ans = s[:i-1] + s[i] + s[i-1]
-        if len(str(int(ans))) != len(s):
-            ans = ""
-    if ans == "":
-        print("-1")
-    else:
+    for i in range(n-1, -1, -1):
+        idx = mind(s, i)
+        if idx > -1:
+            ans = s[:i] + s[idx] + s[i+1:idx] + s[i] + s[idx+1:]
+            break
+
+    if ans != "" and ans != s and ans[0] != "0":
         print(ans)
+    else:
+        print("-1")
