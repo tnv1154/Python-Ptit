88bcf48d842588e29804489d6c08bda7e3bff6ae
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-10 10:44:50 +0700

    10/03/2025

--- PY01026_Sap dat lai xau ky tu.py
+++ PY01026_Sap dat lai xau ky tu.py
@@ -1,1 +1,14 @@
 
+def check(s1, s2):
+    for i in s1:
+        if s1.count(i) != s2.count(i):
+            return False
+    return True
+
+for i in range(int(input())):
+    s1 = input()
+    s2 = input()
+    if check(s1, s2):
+        print("Test " + str(i+1) + ": YES")
+    else:
+        print("Test " + str(i+1) + ": NO")
