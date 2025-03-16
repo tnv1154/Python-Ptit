44fc8a487560f5505c7065ac51e185f4f225447d
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-16 10:44:28 +0700

    13/03/2025

--- PY02012_Sap xep chan le.py
+++ PY02012_Sap xep chan le.py
@@ -1,1 +1,29 @@
 
+n = int(input())
+a = []
+while len(a) < n:
+    a.extend(list(map(int, input().split())))
+
+chan = []
+le = []
+vitri = []#chẵn = 1, lẻ = 0
+
+for i in range(n):
+    if a[i] % 2 == 0:
+        chan.append(a[i])
+        vitri.append(1)
+    else:
+        le.append(a[i])
+        vitri.append(0)
+
+chan = sorted(chan)
+le = sorted(le, reverse=True)
+
+i, j = 0, 0
+for x in range(n):
+    if vitri[x] == 1:
+        print(chan[i], end = " ")
+        i += 1
+    else:
+        print(le[j], end = " ")
+        j += 1
