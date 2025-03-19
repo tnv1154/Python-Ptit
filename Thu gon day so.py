f792cc544b62af2cbca530b201b543a7e2f5737a
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-19 21:03:00 +0700

    19/03/2025

---
+++ Thu gon day so.py
@@ -0,0 +1,12 @@
+def reduce_array(arr):
+    stack = []
+    for num in arr:
+        if stack and (stack[-1] + num) % 2 == 0:
+            stack.pop()
+        else:
+            stack.append(num)
+    return len(stack)
+
+n = int(input())
+arr = list(map(int, input().split()))
+print(reduce_array(arr))
