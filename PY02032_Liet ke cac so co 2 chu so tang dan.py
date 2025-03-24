704538d60d6731540d74214776e33ecfdf3c3e98
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-24 11:04:46 +0700

    24/03/2025

--- PY02032_Liet ke cac so co 2 chu so tang dan.py
+++ PY02032_Liet ke cac so co 2 chu so tang dan.py
@@ -1,1 +1,13 @@
 
+se = set()
+s = input()
+n = len(s)
+if len(s) % 2 != 0:
+    n -= 1
+for i in range(0, n, 2):
+    ss = s[i] + s[i + 1]
+    tmp = int(ss)
+    se.add(tmp)
+l = sorted(list(se))
+for x in l:
+    print(x, end=" ")
