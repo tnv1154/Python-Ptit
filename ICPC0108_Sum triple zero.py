321a07314c7cacc93cd2de3e93c82b2c4f52f6fd
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-24 10:06:36 +0700

    24/03/2025

--- ICPC0108_Sum triple zero.py
+++ ICPC0108_Sum triple zero.py
@@ -1,1 +1,19 @@
 
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a = sorted(a)
+    cnt = 0
+    for i in range(len(a)-2):
+        l = i + 1
+        r = n - 1
+        while l < r:
+            s = a[i] + a[l] + a[r]
+            if s == 0:
+                cnt += 1
+                l += 1
+            elif s > 0:
+                r -= 1
+            else:
+                l += 1
+    print(cnt)
