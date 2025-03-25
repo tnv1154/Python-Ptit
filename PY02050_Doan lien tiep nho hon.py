bc9c2c7b09d50b4a0432d75b95e8474a9fa6b3aa
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-25 10:20:17 +0700

    25/03/2025

--- PY02050_Doan lien tiep nho hon.py
+++ PY02050_Doan lien tiep nho hon.py
@@ -2,13 +2,9 @@
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-    print("1", end=" ")
+    d = [1] * n
     for i in range(1,n):
         cnt = 1
-        for j in a[i-1:0:-1]:
-            if j <= a[i]:
-                cnt += 1
-            else:
-                break
-        print(cnt, end=" ")
+        if a[i] >= a[i-1]:
+            d[i-1] += cnt
     print()
