db58f54398bf3e52e65c1caa255ca88baeefc6fe
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-26 23:11:46 +0700

    26/3/2025

--- PY04002_Lop rectangle.py
+++ PY04002_Lop rectangle.py
@@ -1,25 +1,21 @@
-
+import sys
 class Rectangle:
-    def __init__(self, length, width, Color) -> None:
-        self.length = length
-        self.width = width
-        self.Color = Color
-    def perimeter(self):
-        if self.length <= 0 or self.width <= 0:
-            return "INVALID"
-        return 2 * (self.length + self.width)
-    def area(self):
-        if self.length <= 0 or self.width <= 0:
-            return "INVALID"
-        return self.length * self.width
-    def color(self):
-        if self.length <= 0 or self.width <= 0:
-            return "INVALID"
-        return str(self.Color[0].upper() + self.Color[1:].lower())
+    def __init__(self, a, b, c) -> None:
+        self.perimeter = (a+b)*2
+        self.area = a*b
+        self.color = c[:1].upper() + c[1:].lower()
 
-
+arr = input().split() 
+if int(arr[0]) > 0 and int(arr[1]) > 0: 
+    r = Rectangle(int(arr[0]), int(arr[1]), arr[2]) 
+    print('{} {} {}'.format(r.perimeter, r.area, r.color)) 
+else: print('INVALID')
+sys.exit()
 
 if __name__ == '__main__':
-    arr = input().split()
-    r = Rectangle(int(arr[0]), int(arr[1]), arr[2])
-    print('{} {} {}'.format(r.perimeter(), r.area(), r.color()))
+    t = int(input())
+    while t > 0:
+        arr = input().split()
+        r = Rectangle(int(arr[0]), int(arr[1]), int(arr[2]))
+        print('{} {} {}'.format(r.perimeter(), r.area(), r.color()))
+        t -= 1
