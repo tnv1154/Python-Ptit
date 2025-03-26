9f0e36f7c07b2239dbc93b411d5f07928f234355
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-26 22:40:45 +0700

    26/3/2025

--- PY04001_Lop point.py
+++ PY04001_Lop point.py
@@ -1,4 +1,13 @@
+import math
+from decimal import Decimal
 
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+    def distance(self, b):
+        dis = math.sqrt( (self.x - b.x) ** 2 + (self.y - b.y) ** 2 )
+        return f"{dis:.4f}"
 
 if __name__ == '__main__':
     t = int(input())
