63a26527c468f6b810a867024d2fd0adc9d1beaf
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-26 22:03:56 +0700

    26/3/2025

--- PY04001_Lop point.py
+++ PY04001_Lop point.py
@@ -1,4 +1,11 @@
 
 
-
+if __name__ == '__main__':
+    t = int(input())
+    while t > 0:
+        arr = input().split()
+        p1 = Point(Decimal(arr[0]), Decimal(arr[1]))
+        p2 = Point(Decimal(arr[2]), Decimal(arr[3]))
+        print(p1.distance(p2))
+        t -= 1
 
