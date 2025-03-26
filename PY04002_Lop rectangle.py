ce141470ae4cc19236f92f3a799bbad4c1d7590c
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-26 23:03:55 +0700

    26/3/2025

--- PY04002_Lop rectangle.py
+++ PY04002_Lop rectangle.py
@@ -1,7 +1,25 @@
 
+class Rectangle:
+    def __init__(self, length, width, Color) -> None:
+        self.length = length
+        self.width = width
+        self.Color = Color
+    def perimeter(self):
+        if self.length <= 0 or self.width <= 0:
+            return "INVALID"
+        return 2 * (self.length + self.width)
+    def area(self):
+        if self.length <= 0 or self.width <= 0:
+            return "INVALID"
+        return self.length * self.width
+    def color(self):
+        if self.length <= 0 or self.width <= 0:
+            return "INVALID"
+        return str(self.Color[0].upper() + self.Color[1:].lower())
+
 
 
 if __name__ == '__main__':
     arr = input().split()
-    r = Rectangle(int(arr[0]), int(arr[1]), int(arr[2]))
+    r = Rectangle(int(arr[0]), int(arr[1]), arr[2])
     print('{} {} {}'.format(r.perimeter(), r.area(), r.color()))
