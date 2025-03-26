ce18e4acb8fc9643fcf41f73f2c9a3b6e8a3410b
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-26 22:42:50 +0700

    26/3/2025

--- PY04002_Lop rectangle.py
+++ PY04002_Lop rectangle.py
@@ -1,1 +1,7 @@
 
+
+
+if __name__ == '__main__':
+    arr = input().split()
+    r = Rectangle(int(arr[0]), int(arr[1]), int(arr[2]))
+    print('{} {} {}'.format(r.perimeter(), r.area(), r.color()))
