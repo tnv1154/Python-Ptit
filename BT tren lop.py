1b65a9e696f80f0c8b1a341e39090ddc35a0a733
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-26 21:54:24 +0700

    25/03/2025

--- BT tren lop.py
+++ BT tren lop.py
@@ -1,2 +1,13 @@
-a = "asdfafs" = "s"
-print(a[0])
+
+
+class Restaurant():
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def describe_restaurant(self):
+        print(self.name)
+        print(self.type)
+    def open_restaurant(self):
+        print("open")
+
