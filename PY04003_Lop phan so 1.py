8a9c8275608c7a795298831e9bbc0d73ab647878
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-27 10:01:45 +0700

    27/3/2025

--- PY04003_Lop phan so 1.py
+++ PY04003_Lop phan so 1.py
@@ -1,1 +1,21 @@
 
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+class PhanSo:
+    def __init__(self, tu, mau):
+        self.tu = tu
+        self.mau = mau
+    def rutGon(self):
+        ucln = gcd(self.tu, self.mau)
+        self.tu /= ucln
+        self.mau /= ucln
+
+if __name__ == '__main__':
+    tu, mau = map(int, input().split())
+    p = PhanSo(tu, mau)
+    p.rutGon()
+    print(f"{p.tu:.0f}/{p.mau:.0f}")
