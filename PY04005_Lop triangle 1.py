b6f7419f8b9cd779612c99a000c299b39951db70
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-27 12:25:53 +0700

    27/3/2025

--- PY04005_Lop triangle 1.py
+++ PY04005_Lop triangle 1.py
@@ -1,1 +1,16 @@
+import math
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def distance(self, p):
+        return math.sqrt((self.x - p.x) ** 2 + (self.y - p.y) ** 2)
+
+class Triangle(Point):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
