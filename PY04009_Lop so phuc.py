9a47f03c5a1162b4b6da6d377a5c97d8b8f727bf
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-28 22:34:51 +0700

    28/3/2025

--- PY04009_Lop so phuc.py
+++ PY04009_Lop so phuc.py
@@ -5,7 +5,14 @@ class SoPhuc:
     def __init__(self, thuc, ao):
         self.thuc = thuc
         self.ao = ao
-
+    def cong(self, b):
+        thuc = self.thuc + b.thuc
+        ao = self.ao + b.ao
+        return SoPhuc(thuc, ao)
+    def nhan(self, b):
+        thuc = (self.thuc * b.thuc) - (self.ao * b.ao)
+        ao = (self.thuc * b.ao) + (b.thuc * self.ao)
+        return SoPhuc(thuc, ao)
 
 if __name__ == '__main__':
     inputs = sys.stdin.read().split()
@@ -13,5 +20,5 @@ if __name__ == '__main__':
     for _ in range(int(inputs[0])):
         a, b, c, d = list(map(int, inputs[i:i+4]))
         i += 4
-        x = SoPhuc(a, b)
-        y = SoPhuc(c, d)
+        A = SoPhuc(a, b)
+        B = SoPhuc(c, d)
