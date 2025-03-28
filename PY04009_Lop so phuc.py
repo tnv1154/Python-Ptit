1d8b226ea681af88fa669e2a92f866797e600608
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-03-28 22:51:45 +0700

    28/3/2025

--- PY04009_Lop so phuc.py
+++ PY04009_Lop so phuc.py
@@ -1,4 +1,4 @@
-#import sys
+import sys
 
 class SoPhuc:
     def __init__(self, thuc, ao):
@@ -26,13 +26,11 @@ class SoPhuc:
 
 
 if __name__ == '__main__':
-    """inputs = sys.stdin.read().split()
-    i = 1"""
-    #for _ in range(int(inputs[0])):
-        #a, b, c, d = list(map(int, inputs[i:i+4]))
-        #i += 4
-    for _ in range(int(input())):
-        a, b, c, d = list(map(int, input().split()))
+    inputs = sys.stdin.read().split()
+    i = 1
+    for _ in range(int(inputs[0])):
+        a, b, c, d = list(map(int, inputs[i:i+4]))
+        i += 4
         A = SoPhuc(a, b)
         B = SoPhuc(c, d)
         C = (A.cong(B)).nhan(A)
