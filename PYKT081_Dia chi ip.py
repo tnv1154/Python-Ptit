3ef03605e4fe094327d7399873cf6a09aee18f33
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-04-02 22:41:32 +0700

    02/04/2025

--- PYKT081_Dia chi ip.py
+++ PYKT081_Dia chi ip.py
@@ -1,1 +1,19 @@
 
+def check(s):
+    cnt = 0
+    for x in s:
+        if x.isdigit() or x == ".":
+            if x == ".":
+                cnt += 1
+        else:
+            return False
+    if cnt != 3:
+        return False
+
+
+for _ in range(int(input())):
+    s = input().strip()
+    if check(s):
+        print("YES")
+    else:
+        print("NO")
