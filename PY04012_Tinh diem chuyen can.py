4887e473b6aa0e216fc6b0116d3416e0323dba3b
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-04-24 21:41:35 +0700

    24/04/2025

--- PY04012_Tinh diem chuyen can.py
+++ PY04012_Tinh diem chuyen can.py
@@ -1,3 +1,41 @@
 
 class SinhVien:
-    def __init__(self):
+    def __init__(self, msv, ten, lop):
+        self.msv = msv
+        self.ten = " ".join(ten.title().split())
+        self.lop = lop
+        self.chuyenCan = 10
+
+    def TinhDiemChuyenCan(self, diemDanh):
+        for c in diemDanh:
+            if c == "v":
+                self.chuyenCan -= 2
+            elif c == "m":
+                self.chuyenCan -= 1
+        if self.chuyenCan < 0:
+            self.chuyenCan = 0
+
+
+    def __str__(self):
+        return f"{self.msv} {self.ten} {self.lop} {self.chuyenCan} " + ("KDDK" if self.chuyenCan == 0 else "")
+
+def main():
+    SinhViens = []
+    mp = {}
+    cnt = 0
+    n = int(input())
+    for _ in range(n):
+        msv = input().strip()
+        ten = input().strip()
+        lop = input().strip()
+        SinhViens.append(SinhVien(msv, ten, lop))
+        mp[msv] = cnt
+        cnt += 1
+    for i in range(n):
+        msv, diemDanh = map(str, input().split())
+        SinhViens[mp[msv]].TinhDiemChuyenCan(diemDanh)
+    for sv in SinhViens:
+        print(sv)
+
+if __name__ == "__main__":
+    main()
