081320ee825062a2d919a16015b2bfe7a3cc6693
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-05-15 21:58:16 +0700

    25/04/2025

--- PY04018_Xac dinh trung tuyen.py
+++ PY04018_Xac dinh trung tuyen.py
@@ -1,2 +1,31 @@
 
-class
+class GiaoVien:
+    def __init__(self, id, ten, ma, tin, chuyenMon):
+        self.ten = " ".join(ten.split(ten.title()))
+        self.id = f"GV{id}:02d"
+        if ma[0] == "A":
+            self.mon = "TOAN"
+        elif ma[0] == "B":
+            self.mon = "LY"
+        elif ma[0] == "C":
+            self.mon = "HOA"
+
+        uuTien = 0
+        if ma[1] == "1":
+            uuTien = 2.0
+        elif ma[1] == "2":
+            uuTien = 1.5
+        elif ma[1] == "3":
+            uuTien = 1.0
+        else:
+            uuTien = 0
+
+        self.tong = tin * 2 + chuyenMon + uuTien
+        if self.tong >= 18:
+            self.ketQUa = "TRUNG TUYEN"
+        else: self.ketQUa = "LOAI"
+
+    def __str__(self):
+        return f"{self.id} {self.ten} {self.mon} {round(self.tong, 1)} {self.ketQUa}"
+
+
