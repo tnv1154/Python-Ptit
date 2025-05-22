8c6b5b344520e049c227e45da85172119707c5b2
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-05-22 23:41:44 +0700

    22/05/2025

--- PYKT080_Thong ke dich te.py
+++ PYKT080_Thong ke dich te.py
@@ -10,8 +10,11 @@ for row in range(n):
         if a[row][colum] == -1:
             for i in range(row - 1, row + 2):
                 for j in range(colum - 1, colum + 2):
-                    if 0 <= i < n and 0 <= j < m and a[i][j] != -1:
-                        ans += a[i][j]
-                        a[i][j] = 0
+                    if 0 <= i < n and 0 <= j < m and i != row and j != colum:
+                        if a[i][j] == -1:
+                            ans += 1
+                        else:
+                            ans += a[i][j]
+                            a[i][j] = 0
 
 print(ans)
