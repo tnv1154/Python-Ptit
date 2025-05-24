991df7764272eececbc9904cda6e6e806741d379
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-05-24 23:20:15 +0700

    24/05/2025

--- PY01064_Ky tu thu k.py
+++ PY01064_Ky tu thu k.py
@@ -3,7 +3,7 @@ def find(n, k):
     if k == 2**(n - 1):
         return n
     if k > 2**(n - 1):
-        return find(n -1, k - 2**(n - 1))
+        return find(n - 1, k - 2**(n - 1))
     return find(n - 1, k)
 
 for i in range(int(input())):
