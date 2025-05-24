cb56cde112b71c94a737a65eb66afe63d64ad204
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-05-24 23:15:49 +0700

    24/05/2025

--- PY01064_Ky tu thu k.py
+++ PY01064_Ky tu thu k.py
@@ -1,2 +1,11 @@
 
+def find(n, k):
+    if k == 2**(n - 1):
+        return n
+    if k > 2**(n - 1):
+        return find(n -1, k - 2**(n - 1))
+    return find(n - 1, k)
 
+for i in range(int(input())):
+    n, k = map(int, input().split())
+    print(chr(find(n, k) + ord('A') - 1))
