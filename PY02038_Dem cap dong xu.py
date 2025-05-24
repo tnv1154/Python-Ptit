999f31b50a412af6cc4f7fdb170d3f40d2e271d8
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-05-24 23:41:16 +0700

    24/05/2025

--- PY02038_Dem cap dong xu.py
+++ PY02038_Dem cap dong xu.py
@@ -1,1 +1,17 @@
 
+arr = []
+n = int(input())
+for i in range(n):
+    arr.append(list(input().strip()))
+cnt = 0
+for i in range(n):
+    row = 0
+    col = 0
+    for j in range(n):
+        if arr[i][j] == "C":
+            row += 1
+        if arr[j][i] == "C":
+            col += 1
+    cnt += row * (row - 1) // 2
+    cnt += col * (col - 1) // 2
+print(cnt)
